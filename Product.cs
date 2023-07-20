d9e78f5308dd267048c6be77bda22ecedd373ccf
LeeMclafferty <lee.mclafferty@gmail.com>
2023-07-20 10:50:33 -0700

    Finihsed GUIs

---
+++ Product.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_I___C____C968
+{
+    public partial class Product : Form
+    {
+        public Product()
+        {
+            InitializeComponent();
+        }
+
+        private void LblPriceCost_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblMax_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void TbName_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void LblName_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void LblId_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void TbMin_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void TbMax_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void TbPriceCost_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void TbInventory_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void LblMin_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void TbId_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
